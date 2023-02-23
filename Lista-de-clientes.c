3d4f100f10b50c5f4b9c1c61e67a750d9f77e078
LeonardoCardozo616 <leon52.cpere@outlook.com>
2023-02-23 11:40:52 -0300

    Adicionando Comentários

--- Lista-de-clientes.c
+++ Lista-de-clientes.c
@@ -11,29 +11,42 @@ typedef struct tipo_cliente
     struct tipo_cliente *proximo;
 }Tcliente;
 
+/*Criação das listas*/
 void listaEncadeada(Tcliente **cabeca, Tcliente **inicio ,FILE* fp);//Criação de uma lista Encadeada
 Tcliente* listaSequencial(FILE* fp, int linhas, int* tam);//Criação de uma lista Sequencial
+
+/*Funções com retorno.*/
 Tcliente* criaçãoDeNo();//Criação de um nó
 int getRG();//Recebe um valor de RG
 int getPos(int caso, int tam);//Recebe um valor de posição
 int getListaTam(Tcliente *cabeca);//Recebe o tamanho da lista encadeada
+
+/*Inserção de clientes*/
 void inserirClienteInicioE(Tcliente **cabeca, int *C, int *M);//Insere um novo cliente no início da lista Encadeada
 void inserirClienteFinalE(Tcliente **final, int *C, int *M);//Insere um novo cliente no fim da lista Encadeada
 void inserirClienteE(Tcliente **cabeca, int *C, int *M);//Insere um novo cliente em qualquer posição da lista Encadeada
 void inserirClienteInicioS(Tcliente **usuario, int tam, int* C, int *M);//Insere um novo cliente no início da lista Sequencial
 void inserirClienteFinalS(Tcliente **usuario, int tam, int* C, int *M);//Insere um novo cliente no fim da lista Sequencial
 void inserirClienteS(Tcliente **usuario, int tam, int *C, int *M);//Insere um novo cliente em qualquer posição da lista Sequencial
+
+/*Remoção de clientes*/
 void removerClienteInicioE(Tcliente** inicio, int *C, int *M);//Remove o primeiro cliente da lista Encadeada
 void removerClienteFinalE(Tcliente** cabeca, int *C, int *M);//Remove o último cliente da lista Encadeada
 void removerClienteE(Tcliente** cabeca, int *C, int *M);//Remove um cliente de qualquer posição da lista Encadeada
 void removerClienteInicioS(Tcliente** usuario, int tam, int* C, int *M);//Remove o primeiro cliente da lista Sequencial
 void removerClienteFinalS(Tcliente** usuario, int tam, int* C, int* M);//Remove o último cliente da lista Sequencial
 void removerClienteS(Tcliente** usuario, int tam, int *C, int *M);//Remove um cliente de qualquer posição da lista Sequencial
+
+/*Buscar clientes*/
 void buscarClienteE(Tcliente *cabeca, int *C, int *M);// Encontra um cliente em especifico através do RG em uma lista enccadeada
 void buscarClienteS(Tcliente *usuario, int tam, int *C, int *M);// Encontra um cliente em especifico através do RG em uma lista sequencial
 void buscarClienteBinario(Tcliente *usuario, int tam, int *C, int *M);//Encontra um cliente em específico através do RG em uma lista sequencial ordenada
+
+/*Imprimir lista*/
 void imprimirListaE(Tcliente *cabeca);//imprime a lista encadeada
 void imprimirListaS(Tcliente *usuario, int tam);
+
+/*Ordenações*/
 void selectionSort(Tcliente **usuario, int tam, int *C, int *M);
 void insertSort(Tcliente **usuario, int tam, int *C, int *M);
 void bubbleSort(Tcliente **usuario, int tam, int *C, int *M);
@@ -41,8 +54,10 @@ void shellSort(Tcliente **usuario, int tam, int *C, int *M);
 void quickSort(Tcliente **usuario, int inicio, int fim, int *C, int *M);//Ordena com QuickSort
 void mergeSort(Tcliente **usuario, int inicio, int fim, int *C, int *M);// Ordena com MergeSort
 void merge(Tcliente *usuario, int inicio, int meio, int fim, int *C, int *M);// Ordena com Merge
-void lerListaArquivo(FILE *fp, int n, Tcliente *cabeca);// Lê a lista em arquivo encadeado
-void lerListaArquivoSequencial(FILE *fp, int n, int tam, Tcliente *usuario);// Lê a lista em arquivo sequencial
+
+/*Manipulação de arquivos*/
+void lerListaArquivoE(FILE *fp, int n, Tcliente *cabeca);// Lê a lista em arquivo encadeado
+void lerListaArquivoS(FILE *fp, int n, int tam, Tcliente *usuario);// Lê a lista em arquivo sequencial
 
 int main()
 {
@@ -146,10 +161,10 @@ int main()
 
                 case 9: imprimirListaE(inicio); break;//Imprimir Lista
 
-                case 10: lerListaArquivo(file, 9, inicio);//Criar um Arquivo
+                case 10: lerListaArquivoE(file, 9, inicio);//Criar um Arquivo
                         break;
 
-                case 11: lerListaArquivo(file, 10, inicio);//Imprimir Arquivo
+                case 11: lerListaArquivoE(file, 10, inicio);//Imprimir Arquivo
                          break;
 
                 case 12: continue;//Sair
@@ -284,10 +299,10 @@ int main()
                         imprimirListaS(usuario, tam);
                         break;
 
-                case 10: lerListaArquivoSequencial(fileS, 9, tam, usuario);
+                case 10: lerListaArquivoS(fileS, 9, tam, usuario);
                         break;
 
-                case 11:lerListaArquivoSequencial(fileS, 10, tam, usuario);
+                case 11:lerListaArquivoS(fileS, 10, tam, usuario);
                         break;
 
                 case 12: continue;
@@ -865,7 +880,7 @@ void merge(Tcliente *usuario, int inicio, int meio, int fim, int *C, int *M)
     free(R);
 }
 
-void lerListaArquivo(FILE *fp, int n, Tcliente *cabeca)
+void lerListaArquivoE(FILE *fp, int n, Tcliente *cabeca)
 {
     char arq1[TAM], arq2[TAM];
 
@@ -905,7 +920,7 @@ void lerListaArquivo(FILE *fp, int n, Tcliente *cabeca)
     }
 }
 
-void lerListaArquivoSequencial(FILE *fp, int n,int tam,Tcliente *usuario)
+void lerListaArquivoS(FILE *fp, int n,int tam,Tcliente *usuario)
 {
     char arq1[TAM], arq2[TAM];
 
